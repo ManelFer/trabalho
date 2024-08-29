1ebee4579f4d9bb5e2f7379c29d33eed853b5b48
Manoel Ferreira Matos <manoelferreiramatos08@gmail.com>
2024-08-29 15:02:13 -0300

    alteração
    Feita a continuação do codigo 29/08/2024

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 
-function Home(){
-    return(
-        {/* Banner  */},
-        <header className="w-full h-[420px] bg-zinc-900 bg-cover bg-center ">
-            <div className="w-full h-full flex flex-col justify-center items-center">
-                <h1 className="text-4xl mt-4 mb-2 font-bold text-white"> Manoel Ferreira Matos </h1>
-                <span className="text-white font-medium"> 22 years old, Studying, Developer </span>
-                <div className="pt-6">
-                <button type="submit" className="flex space-x-3 font-bold bg-green-500 hover:bg-green-800 text-white px-4 py-1 rounded hover:scale-110"><p> My CV </p></button>
-                </div>
-            </div>
-        </header>
+function Home() {
+  return (
+    {
+      /* Banner  */
+    },
+    (
+      <header className="w-full h-[420px] bg-zinc-900 bg-home bg-cover bg-center ">
+        <div className="w-full h-full flex flex-col justify-center items-center">
+          <h1 className="text-4xl mt-4 mb-2 font-bold text-white">
+            {" "}
+            Manoel Ferreira Matos{" "}
+          </h1>
+          <span className="text-white font-medium">
+            {" "}
+            22 years old, Studying, Developer{" "}
+          </span>
+          <div className="pt-6">
+            <button
+              type="submit"
+              className="flex space-x-3 font-bold bg-green-500 hover:bg-green-800 text-white px-4 py-1 rounded hover:scale-110"
+            >
+              <p> My CV </p>
+            </button>
+          </div>
+        </div>
+      </header>
     )
+  );
 }
-export default Home
+export default Home;
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,21 +1,92 @@
 import React from "react";
+import Python from "../../assets/python.png";
+import Js from "../../assets/js.png";
 
-function Project(){
-    return(
-        <div className="grid grid-cols-3 gap-4">
-
-            <div className="flex gap-2">
-                <img src="" alt="" />
-                <div>
-                    <p className="font-bold"> Project 1</p>
-                    <p className="text-sm"> Descrição do projeto </p>
-                    <div className="flex items-center gap-2 justify-between mt-3">
-                        <button><i src="https://github.com/ManelFer/Cardapio/blob/main/index.html">Git</i></button>
-                    </div>
-                </div>
-            </div>
-
+function Project() {
+  return (
+    <main className="grid grid-cols-3 gap-4 p-2">
+      <div className=" p-2 flex flex-col items-center bg-zinc-600 hover:scale-110 duration-500 border border-white">
+        <img
+          src={Python}
+          alt=""
+          className="w-10 h-10 rounded-md hover:scale-110 hover:-rotate-2 duration-300"
+        />
+        <div className="flex flex-col justify-center items-center">
+          <p className="font-bold text-zinc-300 gap-4">
+            {" "}
+            Sistema de Caixa eletronico{" "}
+          </p>
+          <p className="text-wrap text-zinc-300 gap-2">
+            {" "}
+            Projeto realizado no curso de python da plataforma DIO. Confira o
+            código do sistema de caixa eletronico de banco feito em Python e
+            poo!
+          </p>
+          <div className="flex justify-between gap-2 mt-3">
+            <a
+              href="https://github.com/ManelFer/Sistema_de_caixa"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-gray-900 px-5 rounded add-to-cart-btn text-white "
+            >
+              gitHub
+            </a>
+          </div>
         </div>
-    )
+      </div>
+
+      <div className="p-2 flex flex-col items-center bg-zinc-600 hover:scale-110 duration-500 border border-white">
+        <img
+          src={Js}
+          alt=""
+          className="w-12 h-12 rounded-md hover:scale-110 hover:-rotate-2 duration-300"
+        />
+        <div className="flex flex-col justify-center items-center">
+          <p className="font-bold text-zinc-300 gap-4"> Cardapio Digital </p>
+          <p className="text-wrap text-zinc-300 gap-2">
+            Projeto feito em HTML, CSS, JAVASCRIPT e TAILWINDCSS. Cardapio
+            digital e responsivel, feito para testar meus conhecimento no
+            tailwindcss e javascript!
+          </p>
+          <div>
+            <a
+              href="https://github.com/ManelFer/Cardapio"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-gray-900 px-5  rounded add-to-cart-btn text-white flex justify-end"
+            >
+              gitHub
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className=" p-2 flex flex-col items-center bg-zinc-600 hover:scale-110 duration-500 border border-white">
+        <img
+          src={Python}
+          alt=""
+          className="w-10 h-10 rounded-md hover:scale-110 hover:-rotate-2 duration-300"
+        />
+        <div className="flex flex-col justify-center items-center">
+          <p className="font-bold text-zinc-300 gap-4">Sistema de Login</p>
+          <p className="text-wrap text-zinc-300 gap-2">
+            {" "}
+            Sistema de login usando Python e sqLite Nesse sistema podemos usar
+            registro e login!
+          </p>
+          <div className="flex justify-between gap-2 mt-3">
+            <a
+              href="https://github.com/ManelFer/Sistema_de_caixa"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-gray-900 px-5 rounded add-to-cart-btn text-white "
+            >
+              gitHub
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
-export default Project
+export default Project;
