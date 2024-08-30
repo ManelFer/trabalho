ff09f4364bf1ee9f7be7c42052bf297b27bec94b
Manoel Ferreira Matos <manoelferreiramatos08@gmail.com>
2024-08-30 16:15:05 -0300

    mudança
    30/08/2024
    feita alteração do about

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -10,18 +10,18 @@ function About() {
           {" "}
           Welcome to
         </h1>
-        <h2 className="flex justify-center font-bold text-white text-2xl">
+        <h2 className="flex justify-center font-bold text-white text-2xl mb-2">
           {" "}
           my website{" "}
         </h2>
-        <div className="bg-black w-2/4">
-          <p className="text-white mb-2">
+        <div className="w-full mx-auto">
+          <p className="text-white mb-2 text-xl">
             {" "}
             ------Hello, my name is Manoel Ferreira Matos, but you can just call
             me Manoel. I'm 21 yeas old and currently working with technical
             support and studying systms analysis and development.{" "}
           </p>
-          <p className="text-white">
+          <p className="text-white text-xl">
             ------Regarding my personal tastes, i really like hiking and playing
             on the computer and also developing systems and websites. I'm very
             passionate aboute coffe, i have three coffe tattoos.
