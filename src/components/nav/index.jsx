2b5b02520c60e418e8c50f3756435b2070e9794e
Manoel Ferreira Matos <manoelferreiramatos08@gmail.com>
2024-09-03 15:53:00 -0300

    alteração
    03/09/2024 Mudado o navbar

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,66 +1,25 @@
-import React, { useState } from "react";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Navbar() {
-  const [nav, setNav] = useState(false);
-
-  const handleNav = () => {
-    setNav(!nav);
-  };
-
-  const navItems = [
-    { id: 1, text: "Home" },
-    { id: 2, text: "About" },
-    { id: 3, text: "Project", anchor: "Project" },
-    { id: 4, text: "Contact" },
-  ];
+function Nav() {
   return (
-    <div className=" bg-black flex justify-between items-center h-24 max-w-[1920px] mx-auto px-4 text-white">
-      <h1 className="w-full text-2xl font-bold text-white">
-        Manoel Ferreira Matos
-      </h1>
-
-      {/* Desktop navigation */}
-      <ul className="hidden md:flex">
-        {navItems.map((item) => (
-          <li
-            key={item.id}
-            className="p-4 hover:bg-green-600 font-bold rounded-xl m-2 cursor-pointer duration-300 hover:text-white"
-          >
-            {item.text}
-          </li>
-        ))}
+    <nav className="bg-zinc-900 flex justify-between items-center py-4 p-2">
+      <h1 className="text-white font-bold"> Manoel Ferreira Matos </h1>
+      <ul className="flex flex-row space-x-4">
+        <li className="font-bold hover:bg-green-700 hover:scale-110 duration-300 rounded p-1 text-white spa">
+          <a href="#">Home</a>
+        </li>
+        <li className="font-bold hover:bg-green-700 hover:scale-110 duration-300 rounded p-1 text-white spa">
+          <a href="#">About</a>
+        </li>
+        <li className="font-bold hover:bg-green-700 hover:scale-110 duration-300 rounded p-1 text-white spa">
+          <a href="#">Project</a>
+        </li>
+        <li className="font-bold hover:bg-green-700 hover:scale-110 duration-300 rounded p-1 text-white spa">
+          <a href="#Contact">Contact</a>
+        </li>
       </ul>
-
-      {/* Mobile Navigation Icon */}
-      <div onClick={handleNav} className="block md:hidden">
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
-
-      {/* Mobile Navigation Menu */}
-      <ul
-        className={
-          nav
-            ? "fixed md:hidden left-0 top-0 w-[40%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
-        }
-      >
-        {/* MObile logo */}
-        <h1 className="w-full text-3xl font-bold text-green-600 m-4">
-          {" "}
-          Manoel{" "}
-        </h1>
-        {/* MObile navigate Items */}
-        {navItems.map((item) => (
-          <li
-            key={item.id}
-            className="p-4 border-b rounded-xl hover:bg-green-600 duration-300 hover:text-black cursor-pointer border-gray-600"
-          >
-            {item.text}
-          </li>
-        ))}
-      </ul>
-    </div>
+    </nav>
   );
 }
-export default Navbar;
+export default Nav;
