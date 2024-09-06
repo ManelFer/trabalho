1b7cc2c271e068f2508339b4e470a22b1f392704
Manoel Ferreira Matos <manoelferreiramatos08@gmail.com>
2024-09-06 15:44:15 -0300

    alteração
    06/09/2024 15:43.
    Feita a melhoria no Project e colocado responsivo

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -12,46 +12,70 @@ function Project() {
         </h2>
         <span className="line"></span>
         <div className="content grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-8">
-          <div className="card border border-gray-200 rounded-lg shadow-md p-4">
+          <div className=" card border border-gray-200 rounded-lg shadow-md p-4 flex flex-col items-center">
             <img
               src={Python}
               alt="project img"
-              className="w-40 rounded-md p-4 ml-1"
+              className="w-40 rounded-md p-4 mb-4"
             />
-            <p className="text-gray-600 text-lg mb-4">
-              {" "}
-              Descrição do prpojeto{" "}
-            </p>
-            <p>
+            <p className="text-gray-600 text-lg mb-4"> Descrição do projeto </p>
+            <p className="p-2">
               <span className="text-primary font-bold"> Caixa eletronico</span>
             </p>
-            <p>Colocar aqui um botão de direcionamento</p>
+            <div className="mt-auto hover:scale-110 duration-300">
+              <a
+                href="https://github.com/ManelFer/Sistema_de_caixa"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-zinc-900 hover:bg-zinc-700 text-white font-bold py-2 px-4 rounded"
+              >
+                GitHub
+              </a>
+            </div>
           </div>
 
-          <div className="card border border-gray-200 rounded-lg shadow-md p-4">
+          <div className="card border border-gray-200 rounded-lg shadow-md p-4 flex flex-col items-center">
             <img
               src={Js}
               alt="Project img"
-              className="w-40 rounded-md p-4 ml-1"
+              className="w-40 rounded-md p-4 mb-4"
             />
             <p className="text-gray-600 text-lg mb-4"> Descrição do projeto</p>
-            <p>
+            <p className="pb-2">
               <span className="text-primary font-bold"> Trabalho </span>
             </p>
-            <p>Colocar aqui um botão de direcionamento</p>
+            <div className="mt-auto hover:scale-110 duration-300">
+              <a
+                href="https://github.com/ManelFer/trabalho"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-zinc-900 hover:bg-zinc-700 text-white font-bold py-2 px-4 rounded"
+              >
+                GitHub
+              </a>
+            </div>
           </div>
 
-          <div className=" card border border-gray-200 rounded-lg shadow-md p-4">
+          <div className=" card border border-gray-200 rounded-lg shadow-md p-4 flex flex-col items-center">
             <img
               src={Python}
               alt="Project img"
               className=" w-40 rounded-md p-4 ml-2"
             />
             <p className=" text-gray-600 text-lg mb-4"> Descrição do projeto</p>
-            <p>
+            <p className="pb-2">
               <span className="text-primary font-bold"> Sistema de Login </span>
             </p>
-            <p>Colocar aqui um botão de direcionamento</p>
+            <div className="mt-auto hover:scale-110 duration-300">
+              <a
+                href="https://github.com/ManelFer/SistemaLogin"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-zinc-900 hover:bg-zinc-700 text-white font-bold py-2 px-4 rounded"
+              >
+                GitHub
+              </a>
+            </div>
           </div>
         </div>
       </div>
