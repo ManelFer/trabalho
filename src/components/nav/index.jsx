7536572cfb9cc7c8e47d0b8bf6c910bdfb73be8c
Manoel Ferreira Matos <manoelferreiramatos08@gmail.com>
2024-09-06 11:10:31 -0300

    Alteração
    06/09/2024 11:10
    Feita a mudança na responsividade das grids

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -6,12 +6,12 @@ function Nav() {
   const closeMenu = () => setClick(false);
 
   return (
-    <nav className="bg-gradient-to-r from-zinc-800 to-zinc-700 flex justify-between items-center py-2 p-2 border-b-2 border-zinc-300 ">
-      <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-zinc-400 to-zinc-300 font-bold text-1xl md:text-3xl">
+    <nav className=" bg-gradient-to-r from-zinc-800 to-zinc-700 flex justify-between items-center py-2 p-2 border-b-2 border-zinc-300 ">
+      <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-zinc-400 to-zinc-300 font-bold text-1xl md:text-3xl pl-10">
         {" "}
         Manoel Ferreira Matos{" "}
       </h1>
-      <ul className={`flex flex-row space-x-4 pr-4 ${click ? "block" : ""}`}>
+      <ul className={`flex flex-row space-x-4 pr-10 ${click ? "block" : ""}`}>
         <li className="font-bold hover:bg-green-700 hover:scale-110 duration-300 rounded p-1 text-white">
           <Link
             to="Home"
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -4,85 +4,54 @@ import Js from "../../assets/js.png";
 
 function Project() {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-3" id="Project">
-      <div className=" p-2 flex flex-col items-center bg-zinc-600 hover:scale-110 duration-500 border">
-        <img
-          src={Python}
-          alt=""
-          className="w-10 h-10 rounded-md hover:scale-110 hover:-rotate-2 duration-300"
-        />
-        <div className="flex flex-col justify-center items-center">
-          <p className="font-bold text-zinc-300 gap-4"> ATM System </p>
-          <p className="text-wrap text-zinc-300 gap-2">
-            {" "}
-            Project carried out in the DIO platform python course. Check out the
-            bank ATM system code made in Python and poo!
-          </p>
-          <div className="flex justify-between gap-2 mt-3">
-            <a
-              href="https://github.com/ManelFer/Sistema_de_caixa"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-gray-900 hover:bg-green-900 px-5 hover: rounded add-to-cart-btn text-white "
-            >
-              gitHub
-            </a>
+    <div className="w-full">
+      <div className="container w-full md:w-[1240px] mx-auto p-4 md:p-8">
+        <h2 className="font-bold pb-8 text-1xl md:text-3xl text-center">
+          {" "}
+          Projects{" "}
+        </h2>
+        <span className="line"></span>
+        <div className="content grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-8">
+          <div className="card border border-gray-200 rounded-lg shadow-md p-4">
+            <img
+              src={Python}
+              alt="project img"
+              className="rounded-full -mt-8"
+            />
+            <p className="text-gray-600 text-lg mb-4">
+              {" "}
+              Descrição do prpojeto{" "}
+            </p>
+            <p>
+              <span className="text-primary font-bold"> Caixa eletronico</span>
+            </p>
+            <p>Colocar aqui um botão de direcionamento</p>
           </div>
-        </div>
-      </div>
 
-      <div className="p-2 flex flex-col items-center bg-zinc-600 hover:scale-110 duration-500 border">
-        <img
-          src={Js}
-          alt=""
-          className="w-12 h-12 rounded-md hover:scale-110 hover:-rotate-2 duration-300"
-        />
-        <div className="flex flex-col justify-center items-center">
-          <p className="font-bold text-zinc-300 gap-4"> Digital menu </p>
-          <p className="text-wrap text-zinc-300 gap-2">
-            Project made in HTML, CSS, JAVASCRIPT and TAILWINDCSS. Menu digital
-            and responsive, made to test my knowledge in tailwindcss and
-            javascript!
-          </p>
-          <div>
-            <a
-              href="https://github.com/ManelFer/Cardapio"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-gray-900 hover:bg-green-900 px-5  rounded add-to-cart-btn text-white flex justify-end"
-            >
-              gitHub
-            </a>
+          <div className="card border border-gray-200 rounded-lg shadow-md p-4">
+            <img src={Js} alt="Project img" className="rounded-full -mt-8" />
+            <p className="text-gray-600 text-lg mb-4"> Descrição do projeto</p>
+            <p>
+              <span className="text-primary font-bold"> Trabalho </span>
+            </p>
+            <p>Colocar aqui um botão de direcionamento</p>
           </div>
-        </div>
-      </div>
 
-      <div className=" p-2 flex flex-col items-center bg-zinc-600 hover:scale-110 duration-500 border ">
-        <img
-          src={Python}
-          alt=""
-          className="w-10 h-10 rounded-md hover:scale-110 hover:-rotate-2 duration-300"
-        />
-        <div className="flex flex-col justify-center items-center">
-          <p className="font-bold text-zinc-300 gap-4">Login System</p>
-          <p className="text-wrap text-zinc-300 gap-2">
-            {" "}
-            Login system using Python and sqLite In this system we can use
-            registration and login!
-          </p>
-          <div className="flex justify-between gap-2 mt-3">
-            <a
-              href="https://github.com/ManelFer/Sistema_de_caixa"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-gray-900 hover:bg-green-900 px-5 rounded add-to-cart-btn text-white "
-            >
-              gitHub
-            </a>
+          <div className="card border border-gray-200 rounded-lg shadow-md p-4">
+            <img
+              src={Python}
+              alt="Project img"
+              className="rounded-full -mt-8"
+            />
+            <p className="text-gray-600 text-lg mb-4"> Descrição do projeto</p>
+            <p>
+              <span className="text-primary font-bold"> Sistema de Login </span>
+            </p>
+            <p>Colocar aqui um botão de direcionamento</p>
           </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
 export default Project;
