e6235d37ab9349dae268d7a897c644a137cdbf86
Manoel Ferreira Matos <manoelferreiramatos08@gmail.com>
2024-09-09 16:04:05 -0300

    validação
    16:03 09/09/2024
    validação do email terminar

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # Site de Portfólio
+
 Projeto realizado para a matéria de Front-end.
 
 ## Sobre
+
 Desenvolvi um site pessoal, também conhecido como portfólio, utilizando a biblioteca JavaScript React. O objetivo foi criar um site simples, mas que demonstre minhas habilidades e conhecimentos em React.
 
 ## Tecnologias Utilizadas
-* **React**: Biblioteca JavaScript para construir a interface do usuário
-* **Tailwind CSS**: Framework de CSS para estilizar e layout
+
+- **React**: Biblioteca JavaScript para construir a interface do usuário
+- **Tailwind CSS**: Framework de CSS para estilizar e layout
 
 ## Iniciando
+
 Para executar este projeto, siga estes passos:
 
 1. Clone o repositório: `git clone https://github.com/ManelFer/trabalho.git`
@@ -16,15 +20,18 @@ Para executar este projeto, siga estes passos:
 3. Inicie o servidor de desenvolvimento: `npm start` ou `yarn start`
 
 ## Recursos
-* Rolar suave para seções
-* Design responsivo usando Tailwind CSS
+
+- Rolar suave para seções
+- Design responsivo usando Tailwind CSS
 
 ## O que ainda precisa ser feito
-* Fazer o efeito de rolagem ainda é um problema a ser corrigido
-* Fazer o contato funcional
+
+- Fazer o contato funcional
 
 ## Contribuindo
+
 Contribuições são bem-vindas! Se encontrar algum problema ou tiver sugestões, por favor abra uma issue ou envie um pull request.
 
 ## Licença
+
 Este projeto é licenciado sob a Licença MIT.
--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Img from "../../assets/contact.png";
+import emailjs from "emailjs";
 
 function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState({ name: "", email: "" });
+  const validate = () => {
+    let isValid = true;
+    const errors = { name: "", email: "" };
+    if (!name) {
+      errors.name = "O nome é obrigatorio";
+      isValid = false;
+    } else if (name.length < 3) {
+      errors.name = "O nome deve ter mais de 3 caracteres";
+      isValid = false;
+    }
+    if (!email) {
+      errors.email = "O email é obrigatorio";
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = "Email inválido";
+      isValid = false;
+    }
+    setErrors(errors);
+    return isValid;
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validate()) {
+      const serviceId = "service_01x764k";
+      const templateId = "template_qcgw8b9";
+      const userId = "";
+    }
+  };
+
   return (
     <div
       className="flex flex-col justify-between bg-zinc-600 p-4 md:flex-row md:items-center"
@@ -12,7 +45,11 @@ function Contact() {
           {" "}
           Contact{" "}
         </h1>
-        <form className="flex flex-col w-full" action="">
+        <form
+          className="flex flex-col w-full"
+          action=""
+          onSubmit={handleSubmit}
+        >
           <label className="block mb-2 text-white" htmlFor="name">
             {" "}
             Nome:{" "}
@@ -20,10 +57,12 @@ function Contact() {
           <input
             type="text"
             id="name"
+            onChange={(e) => setName(e.target.value)}
             name="name"
             placeholder="Seu nome"
             className="w-full p-2 mb-4 rounded-md"
           />
+          {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
 
           <label htmlFor="email" className="block mb-2 text-white">
             {" "}
@@ -33,9 +72,11 @@ function Contact() {
             type="text"
             id="email"
             name="email"
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Seu e-mail"
             className="w-full p-2 mb-4 rounded-md"
           />
+          {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
 
           <label htmlFor="message" className="block mb-2 text-white">
             {" "}
