e9af44f842f80834b69ab0282145e32ab2dcd423
Manoel Ferreira Matos <manoelferreiramatos08@gmail.com>
2024-09-17 15:58:50 -0300

    alteração
    15:58 17/09/2024
    Alteração na barra de navegação

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -6,12 +6,24 @@ function Nav() {
   const closeMenu = () => setClick(false);
 
   return (
-    <nav className=" bg-gradient-to-r from-zinc-800 to-zinc-700 flex justify-between items-center py-2 p-2 border-b-2 border-zinc-300 ">
-      <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-zinc-400 to-zinc-300 font-bold text-1xl md:text-3xl pl-10">
+    <nav className="bg-gradient-to-r from-zinc-800 to-zinc-700 flex justify-between items-center py-2 p-2 border-b-2 border-zinc-300">
+      <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-zinc-400 to-zinc-300 font-bold text-xl md:text-3xl lg:text-4xl xl:text-5xl pl-10">
         {" "}
         Manoel Ferreira Matos{" "}
       </h1>
-      <ul className={`flex flex-row space-x-4 pr-10 ${click ? "block" : ""}`}>
+      <button
+        className="md:hidden flex justify-center items-center w-8 h-8 bg-gray-800 text-white rounded-full"
+        aria-label="Menu"
+        aria-expanded={click}
+        onClick={() => setClick(!click)}
+      >
+        <i className="fas fa-bars" />
+      </button>
+      <ul
+        className={`flex flex-row space-x-4 pr-10 md:flex md:justify-center md:items-center ${
+          click ? "block" : "hidden"
+        }`}
+      >
         <li className="font-bold hover:bg-green-700 hover:scale-110 duration-300 rounded p-1 text-white">
           <Link
             to="Home"
